f542f965d915940127cf7cdff89b490ad7f94d55
williandeob <williandeob@gmail.com>
2016-12-13 18:50:27 -0200

    criação da estrutura da schedule

--- AtualizadorItecCliente/src/main/java/br/com/itecbrazil/atualizadoriteccliente/App.java
+++ AtualizadorItecCliente/src/main/java/br/com/itecbrazil/atualizadoriteccliente/App.java
@@ -2,8 +2,12 @@ package br.com.itecbrazil.atualizadoriteccliente;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import br.com.itecbrazil.atualizadoriteccliente.model.Configuracao;
+import br.com.itecbrazil.atualizadoriteccliente.model.ThreadAtualizadorDeArquivosDoWebService;
 import br.com.itecbrazil.atualizadoriteccliente.util.UtilBancoDeDados;
 import br.com.itecbrazil.atualizadoriteccliente.util.UtilDirectory;
 
@@ -35,6 +39,9 @@ public class App
     
     private static void iniciarServico(){
     	System.out.println("Iniciou Servico");
+    	ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+    	Runnable thread = new ThreadAtualizadorDeArquivosDoWebService();
+    	service.scheduleAtFixedRate(new Thread(thread), 10, 10, TimeUnit.SECONDS);
     }
     
 	private static void iniciarConfiguracao(){
---
+++ AtualizadorItecCliente/src/main/java/br/com/itecbrazil/atualizadoriteccliente/model/ThreadAtualizadorDeArquivosDoWebService.java
@@ -0,0 +1,10 @@
+package br.com.itecbrazil.atualizadoriteccliente.model;
+
+public class ThreadAtualizadorDeArquivosDoWebService implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("thread executada...");
+	}
+
+}
